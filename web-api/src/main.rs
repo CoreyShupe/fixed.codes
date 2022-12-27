b1ab431e21724de7f9d881dd83a4083363723422
Corey Shupe <coreyshupe101@gmail.com>
2022-12-26 20:30:15 -0500

    Bind api server to 3000 to enable proper boot cycle.

--- web-api/src/main.rs
+++ web-api/src/main.rs
@@ -1,8 +1,10 @@
+use actix_web::{App, HttpServer};
+
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
     web_commons::logger::attach_default_system_logger()?;
 
-    // todo - web api service
+    HttpServer::new(|| App::new()).bind("0.0.0.0:3000")?;
 
     Ok(())
 }
