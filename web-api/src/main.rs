01a59921c30e0fbd78a12ca23cd778f8b919f52a
Corey Shupe <coreyshupe101@gmail.com>
2022-12-26 20:56:07 -0500

    Bind / for get and head

--- web-api/src/main.rs
+++ web-api/src/main.rs
@@ -1,10 +1,18 @@
-use actix_web::{App, HttpServer};
+use actix_web::{route, App, HttpResponse, HttpResponseBuilder, HttpServer};
+
+#[route("/", method = "HEAD", method = "GET")]
+async fn index_ok() -> HttpResponseBuilder {
+    HttpResponse::Ok()
+}
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
     web_commons::logger::attach_default_system_logger()?;
 
-    HttpServer::new(|| App::new()).bind("0.0.0.0:3000")?;
+    HttpServer::new(|| App::new().service(index_ok))
+        .bind(("0.0.0.0", 3000))?
+        .run()
+        .await?;
 
     Ok(())
 }
