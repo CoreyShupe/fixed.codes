43683fc15d3ec81277e669fe7ce285bb2207fce9
Corey Shupe <coreyshupe101@gmail.com>
2023-02-04 05:50:57 -0500

    Major updates to web deploy flow.

---
+++ deploy/elm.Dockerfile
@@ -0,0 +1,20 @@
+FROM wunsh/alpine-elm:latest as builder
+
+RUN mkdir /app
+
+ARG APP_NAME
+
+WORKDIR /app
+COPY $APP_NAME/elm.json .
+COPY $APP_NAME/src src
+
+RUN elm make src/Main.elm
+
+FROM nginx
+
+RUN mkdir /app
+
+COPY --from=builder /app/index.js /app/index.js
+COPY nginx.conf /etc/nginx/nginx.conf
+
+CMD ["nginx"]
---
+++ web/src/Main.elm
@@ -0,0 +1,57 @@
+module Main exposing (..)
+
+import Browser exposing (Document)
+import Browser.Navigation as Nav
+import Html exposing (div)
+import Url
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+type alias Model =
+    {}
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ _ _ =
+    ( {}, Cmd.none )
+
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked _ ->
+            ( model, Cmd.none )
+
+        UrlChanged _ ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+view : Model -> Document Msg
+view _ =
+    { title = "FiXed Codes"
+    , body =
+        [ div []
+            []
+        ]
+    }
