55cf9ccd7e866b41291a39bc8e72dad116699604
Corey Shupe <coreyshupe101@gmail.com>
2023-02-04 11:06:50 -0500

    Change detections as objects

--- scripts/process-service.sh
+++ scripts/process-service.sh
@@ -3,6 +3,8 @@
 # This script simply processes a template; generating the output file
 # and removing/checking the path detections as a result
 
+rm -rf outputs/*
+
 jq -c "del(.path_detections)" $1/$2 > outputs/$1.json
 
 # Send the path detections up the line for vis
