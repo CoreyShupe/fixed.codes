648cbf94c2bab53a552c41222726ae16755578c1
Corey Shupe <coreyshupe101@gmail.com>
2023-02-05 05:14:42 -0500

    Fix default rendering

--- static-proxy/src/main.rs
+++ static-proxy/src/main.rs
@@ -1,3 +1,5 @@
+use actix_files::NamedFile;
+use actix_web::dev::{fn_service, ServiceRequest, ServiceResponse};
 use actix_web::{App, HttpServer};
 
 #[actix_web::main]
@@ -5,7 +7,14 @@ async fn main() -> anyhow::Result<()> {
     web_commons::logger::attach_default_system_logger()?;
 
     HttpServer::new(|| {
-        let files = actix_files::Files::new("/", "/app/static").index_file("index.html");
+        let files = actix_files::Files::new("/", "/app/static")
+            .index_file("index.html")
+            .default_handler(fn_service(|req: ServiceRequest| async {
+                let (req, _) = req.into_parts();
+                let file = NamedFile::open_async("/app/static/index.html").await?;
+                let res = file.into_response(&req);
+                Ok(ServiceResponse::new(req, res))
+            }));
         App::new().service(files)
     })
     .bind(("0.0.0.0", 8080))?
